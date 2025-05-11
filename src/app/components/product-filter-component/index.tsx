36ce2a47050141a29a13d34f5c960bf86b215b16
AlexiaCazale <alexiacazale7@gmail.com>
2025-05-11 00:01:10 -0300

    style: estilizando filtro de produtos

--- src/app/components/product-filter-component/index.tsx
+++ src/app/components/product-filter-component/index.tsx
@@ -1,12 +1,155 @@
+import { Radio, Square, Truck } from "@phosphor-icons/react"
 import styles from "./styles.module.scss"
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Checkbox } from "@mui/material"
+import { useState } from "react";
+import React from "react";
 
 export function ProductFilter() {
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
     return (
-        <div className={styles.content}>
+        <>
             <div className={styles.bar}>
-            <h2>Filtros</h2>
+                <div className={styles.content}>
+                    <h2>Filtros</h2>
+                    <div className={styles.newSection}>
+                        <Truck size={18} />
+                        <p>Frete grátis</p>
+                    </div>
+                    <div>
+                        <FormControl>
+                            <FormLabel className={styles.title}>Categoria</FormLabel>
+
+                            <FormControlLabel value="female" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+
+                            <FormControlLabel value="male" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+                            <FormControlLabel value="female" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Female" />
+                            <FormControlLabel value="male"  sx={{marginLeft: 1}}control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+                            <FormControlLabel value="female" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Female" />
+                            <FormControlLabel value="male"  sx={{marginLeft: 1}}control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+                        </FormControl>
+                    </div>
+                    <div>
+                        <FormControl>
+                            <FormLabel className={styles.title}>Marca</FormLabel>
+
+                            <FormControlLabel value="female" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Female" />
+                            <FormControlLabel value="male"  sx={{marginLeft: 1}}control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+                            <FormControlLabel value="other" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Other" />
+                            <FormControlLabel value="female" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Female" />
+                            <FormControlLabel value="male"  sx={{marginLeft: 1}}control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+                            <FormControlLabel value="other" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Other" />
+                        </FormControl>
+                    </div>
+                    <div>
+                        <FormControl>
+                            <FormLabel className={styles.title}>Entrega</FormLabel>
+
+                            <FormControlLabel value="female" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Female" />
+                            <FormControlLabel value="male"  sx={{marginLeft: 1}}control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+                            <FormControlLabel value="other" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Other" />
+                            <FormControlLabel value="female" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Female" />
+                            <FormControlLabel value="male"  sx={{marginLeft: 1}}control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Male" />
+                            <FormControlLabel value="other" sx={{marginLeft: 1}} control={<Checkbox
+                                size="small"
+                                checked={checked}
+                                onChange={handleChange} />
+
+                            } label="Other" />
+                        </FormControl>
+                    </div>
+                </div>
+
 
             </div>
-        </div>
+        </>
     )
 }
