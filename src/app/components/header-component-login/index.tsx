da8e285472925af17557b29b5735b2f7c1909940
AlexiaCazale <alexiacazale7@gmail.com>
2025-05-17 16:32:21 -0300

    fix: corrigindo estilização do header de login/cadastro

--- src/app/components/header-component-login/index.tsx
+++ src/app/components/header-component-login/index.tsx
@@ -6,19 +6,18 @@ import styles from './styles.module.scss'
 export function HeaderComponentLogin() {
     return (
         <div className={styles.content}>
-            {/* <div className={styles.firstRow}> */}
-                <div className={`${styles.logo} container_info`}>
+
+            <div className={`${styles.container} container_info`}>
+                <div className={styles.logo}>
                     <Image src={"/images/logo.png"} alt={''} width={80} height={70}>
                     </Image>
                 </div>
-            
-                <div className={`${styles.profile} container_info`}>
-                  <span>Precisa de 
-                    <br/>
+
+                <span>Precisa de
+                    <br />
                     ajuda?</span>
-                </div>
-            {/* </div> */}
-       
+            </div>
+
         </div>
     )
 }
