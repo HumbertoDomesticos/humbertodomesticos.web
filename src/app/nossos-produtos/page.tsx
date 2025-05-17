d0609b4498b77567ed59a986e9061658ae1a238e
AlexiaCazale <alexiacazale7@gmail.com>
2025-05-17 16:33:32 -0300

    fix: chamando produtos a partir da API em nossos produtos

--- src/app/nossos-produtos/page.tsx
+++ src/app/nossos-produtos/page.tsx
@@ -3,11 +3,13 @@
 import { Box, FormControl, InputLabel, MenuItem, OutlinedInput, Select, type SelectChangeEvent } from "@mui/material";
 import { FooterComponent } from "../components/footer-component";
 import { HeaderComponent } from "../components/header-component";
-import { ProductCard, productList } from "../components/product-card-component";
+import { ProductCard } from "../components/product-card-component";
 import { ProductFilter } from "../components/product-filter-component";
 import styles from './styles.module.scss';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CaretRight, House } from "@phosphor-icons/react";
+import { getProduto, type Produto, type ProdutoApi } from "@/services/routes/produtos/page";
+import Link from "next/link";
 
 export default function NossosProdutos() {
 
@@ -16,6 +18,18 @@ export default function NossosProdutos() {
     const handleChange = (event: SelectChangeEvent) => {
         setAge(event.target.value as string);
     };
+
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+
+    useEffect(() => {
+        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+        getProduto().then((resp: any) => {
+            setProdutos(resp);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, []);
+
 
     return (
         <div>
@@ -30,45 +44,28 @@ export default function NossosProdutos() {
                     </p>
                 </span>
             </div>
-            
+
             <div className={`${styles.content} container_info`}>
 
                 <ProductFilter />
 
                 <div className={styles.productContent}>
                     <div className={styles.firstRow}>
-                        <span>{productList.length} produtos encontrados</span>
-                        <Box sx={{ minWidth: 200 }}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Relevância</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={age}
-                                    label="Age"
-                                    onChange={handleChange}
-                                    input={<OutlinedInput label="Relevância" />}
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Box>
+                        <span>{produtos.length} produtos encontrados</span>
                     </div>
 
                     <div className={styles.products}>
-
-                        {productList.map((product) => (
-                            <div key={product.id} className={styles.productWrapper}>
-                                <ProductCard
-                                    name={product.name}
-                                    price={product.price}
-                                    oldPrice={product.oldPrice}
-                                />
+                        {produtos.map((product) => (
+                            <div key={product.id_prod} className={styles.productWrapper}>
+                                <Link key={product.id_prod} href={`/produto/${product.id_prod}`} passHref>
+                                    <ProductCard
+                                        key={product.id_prod}
+                                        name={product.nome_prod}
+                                        price={product.preco_produto}
+                                    />
+                                </Link>
                             </div>
                         ))}
-
                     </div>
                 </div>
             </div>
