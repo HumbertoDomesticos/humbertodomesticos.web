d391cca7206f7a89cc7ae5ee6ce72d6345ad23da
AlexiaCazale <alexiacazale7@gmail.com>
2025-05-25 18:44:26 -0300

    feat: get usuario por e-mail

--- src/services/routes/usuarios/page.ts
+++ src/services/routes/usuarios/page.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 
 
 export type Usuario = {
-  id_usuario: number;
+  id_usuario?: number;
   nome_usuario: string;
   cpf_usuario: string;
   email_usuario: string;
-  senha_usuario: string;
+  senha_usuario?: string;
   senha_usuario_temp: string;
   data_nasc_usuario: string;
 }
@@ -31,4 +31,9 @@ export async function postUsuario(usuario: Usuario): Promise<AxiosResponse> {
     console.error(error);
     throw error;
   }
+}
+
+export async function getUsuarioPorEmail(email: string): Promise<Usuario[]> {
+  const response = await axios.get(`http://127.0.0.1:8000/usuarios?email=${email}`);
+  return response.data; 
 }
