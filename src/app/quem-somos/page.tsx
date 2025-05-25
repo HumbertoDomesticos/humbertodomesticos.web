138919c1d2c383d563b5c1787c9bd86f65e84300
AlexiaCazale <alexiacazale7@gmail.com>
2025-05-25 18:46:13 -0300

    feat: adicionando mapa em quem somos

--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { FooterComponent } from "../components/footer-component";
 import { HeaderComponent } from "../components/header-component";
 import styles from "./styles.module.scss";
+import MapPage, { DraggableMarker } from "../components/google-maps";
 
 export default function Sobre() {
     return (
@@ -24,7 +25,25 @@ export default function Sobre() {
 
             <div className={`${styles.section} container_info`}>
                 <h1>Humberto Domésticos</h1>
-            </div>
+                <p>O Humberto Domésticos é um marketplace inovador e recém-chegado ao mercado, criado especialmente para facilitar a compra de eletrodomésticos de qualidade. Nosso objetivo é reunir as melhores marcas e produtos em um só lugar, oferecendo uma experiência simples, segura e prática para quem busca renovar sua casa com tecnologia e eficiência.
+                    <br />
+                    Aqui, você encontra desde itens básicos até as últimas novidades em eletrodomésticos, sempre com preços competitivos e condições especiais. Estamos comprometidos em conectar consumidores e vendedores de forma transparente, garantindo variedade, rapidez na entrega e atendimento dedicado.
+                    <br />
+                    Mais do que uma plataforma de vendas, o Humberto Domésticos quer ser um parceiro no dia a dia das pessoas, ajudando a transformar seus lares com soluções inteligentes, sustentáveis e acessíveis.
+
+                </p>
+                {/* <div className={styles.maps} */}
+                    <div>
+                        <h1>Venha nos visitar</h1>
+                        <p>
+                            Nossa sede está localizada na Fatec Jahu, em Jaú/SP — um espaço de inovação e aprendizado que reflete nossos valores de tecnologia, sustentabilidade e compromisso com a comunidade.
+                            Se estiver na região, será um prazer receber você para conhecer mais sobre nossos produtos, ideias e parcerias.
+                        </p>
+                    </div>
+
+                    <MapPage />
+                </div>
+            {/* </div> */}
 
             <div className={`${styles.section} container_info`}>
 
